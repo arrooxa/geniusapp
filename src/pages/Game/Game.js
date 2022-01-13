ba9d0d6974cdcf30d22f15c28caabf9109c04bef
Vitor Rocha <vitor.league@gmail.com>
2022-01-13 01:52:09 -0300

    Tentativa falha, vamo pra amanhã

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Container from '../styles';
 
 import { ButtonHome, ButtonText, GameLabel, LabelGrid, PageText } from './styles'
 
 const Game = ({navigation}) => {
-    const correctGameSequence = [];
+    const [arrayGameSequence, setArrayGameSequence] = useState([]);
 
-    const playerGameSequence = [];
+    const [arrayPlayerSequence, setArrayPlayerSequence] = useState([]);
 
     const [userLost, setUserLost] = useState(false);
 
-    const [blueColor, setLightBlue] = useState(false);
-    const [redColor, setLightRed] = useState(false);
-    const [yellowColor, setLightYellow] = useState(false);
-    const [greenColor, setLightGreen] = useState(false);
+    const [blueColor, setBlueColor] = useState(false);
+    const changeBlueColor = () => setBlueColor(color => !color);
+    const [redColor, setRedColor] = useState(false);
+    const changeRedColor = () => setRedColor(color => !color);
+    const [yellowColor, setYellowColor] = useState(false);
+    const changeYellowColor = () => setYellowColor(color => !color);
+    const [greenColor, setGreenColor] = useState(false);
+    const changeGreenColor = () => setGreenColor(color => !color);
+
 
     const hasUserLost = () => setUserLost(userLost => !userLost);
-
 
     function getRandomInt() {
         return Math.floor(Math.random() * (5 - 1)) + 1;
@@ -25,28 +29,36 @@ const Game = ({navigation}) => {
 
     const nextGameColor = function () {
         const nextColor = getRandomInt();
+        console.log(nextColor);
 
-        if(nextColor === 1) {
-            correctGameSequence.push('blue');
-            setLightBlue(blueColor => !blueColor); // Estudar React Hooks
-        }else if(nextColor === 2){
-            correctGameSequence.push('red');
-            setLightRed(redColor => !redColor); // Estudar React Hooks
-        }else if(nextColor === 3){
-            correctGameSequence.push('yellow');
-            setLightYellow(yellowColor => !yellowColor); // Estudar React Hooks
-        }else{
-            correctGameSequence.push('green');
-            setLightGreen(greenColor => !greenColor); // Estudar React Hooks
+        switch(nextColor) {
+            case 1: 
+                setArrayGameSequence(oldArray => [...oldArray, 1]);
+                changeBlueColor();
+                break;
+            case 2:
+                setArrayGameSequence(oldArray => [...oldArray, 2]);
+                changeRedColor();
+                break;
+            case 3:
+                setArrayGameSequence(oldArray => [...oldArray, 3]);
+                changeYellowColor();
+                break;
+            default:
+                setArrayGameSequence(oldArray => [...oldArray, 4]);
         }
+
+        arrayGameSequence.map((color, index) => {
+            
+        })
     };
 
     function handleGamePress(color) {
         playerGameSequence.push(color);
-        console.log(correctGameSequence);
+        console.log(arrayGameSequence);
         console.log(playerGameSequence);
         
-        if(JSON.stringify(playerGameSequence) === JSON.stringify(correctGameSequence)) {
+        if(JSON.stringify(playerGameSequence) === JSON.stringify(arrayGameSequence)) {
             nextGameColor();
         }else{
             hasUserLost();
@@ -77,23 +89,23 @@ const Game = ({navigation}) => {
                 <LabelGrid>
 
                 <GameLabel 
-                    color={blueColor ? "#0000FF" : "#000066"}
-                    onPressIn={() => handleGamePress('blue')}
+                    color={blueColor ?  "#0000FF" : "#000066"}
+                    onPressIn={() => handleGamePress(1)}
                 />
                 
                 <GameLabel 
                     color={redColor ? "#FF0000" : "#660000"}
-                    onPressIn={() => handleGamePress('red')}
+                    onPressIn={() => handleGamePress(2)}
                 />
                 
                 <GameLabel 
                     color={yellowColor ? "#ffff00" : "#666600"}
-                    onPressIn={() => handleGamePress('yellow')}
+                    onPressIn={() => handleGamePress(3)}
                 />
                 
                 <GameLabel 
                     color={greenColor ? "#00ff00" : "#006600"}
-                    onPressIn={() => handleGamePress('green')}
+                    onPressIn={() => handleGamePress(4)}
                 />
 
             </LabelGrid>
